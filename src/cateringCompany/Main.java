f2dc9b8e9663161fccabda500711d5c16c78c006
laurynxrobson <laurynxrobson@gmail.com>
2022-08-14 12:16:18 +0200

    add more objects

--- src/cateringCompany/Main.java
+++ src/cateringCompany/Main.java
@@ -1,6 +1,5 @@
 package cateringCompany;
 
-import java.util.EventObject;
 import java.util.Scanner;
 
 /**
@@ -16,24 +15,24 @@ public class Main {
     static boolean large_event;
     static String event_ID;
     // Event object
-    static Event eventObj = new Event();
+    static Event event1 = new Event(event_ID, no_of_guests);
+    static Event event2 = new Event(no_of_guests);
 
     public static void main(String[] args) {
-
         // Calling Methods
         carlysMotto();
         guestAmount();
         eventNumber();
         eventPrice();
-
     }
 
     private static void eventPrice() {
-        System.out.println("Number of guest is:\t\t" + eventObj.getGuests());
+        // Event 1
+        System.out.println("Number of guest is:\t\t" + event1.getGuests());
         System.out.println("Price per guest is:\t\t" + price_per_guest);
-        System.out.println("Event ID is:\t\t\t" + eventObj.getEvent_number());
+        System.out.println("Event ID is:\t\t\t" + event1.getEvent_number());
         // Calculates the price of the event
-        total_price = eventObj.getGuests() * price_per_guest;
+        total_price = event1.getGuests() * price_per_guest;
         System.out.println("Total price for the event is:\tt" + total_price);
         // Checks if the event is large
         large_event = no_of_guests >= 50;
@@ -41,17 +40,20 @@ public class Main {
     }
 
     private static void eventNumber() {
+        // Event 1
         input.nextLine();
         // Get the event ID
         System.out.print("Enter the event ID:\t\t");
         event_ID = input.nextLine();
-        eventObj.setEvent_number(event_ID);
+        event1.setEvent_number(event_ID);
+
     }
 
     public static int guestAmount() {
+        // Event 1
         System.out.print("Number of guests:\t\t");
         no_of_guests = input.nextInt();
-        eventObj.setGuests(no_of_guests);
+        event1.setGuests(no_of_guests);
         return no_of_guests;
     }
 
